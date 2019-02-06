afd2c2332d27eaea9518b4554b6150bbc0f096be
terrell oliver <mroliver1995@aol.com>
2019-02-06 10:10:35 -0600

    Added Caclulator class and Add method

---
+++ Calcuator2/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calcuator2
+{
+    class Calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
